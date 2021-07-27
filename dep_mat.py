jacobian calculation eval mode -> fixes zero grads for batchnorm

dep_mat.py
@@ -2,26 +2,37 @@ import torch
 import torch.nn as nn
 from torch.autograd.functional import jacobian
 
-def calc_jacobian(encoder:nn.Module, latents:torch.Tensor, normalize:bool=False)->torch.Tensor:
-    # calculate the jacobian
-    #return B x n_out x n_in
+
+def calc_jacobian(encoder: nn.Module, latents: torch.Tensor, normalize: bool = False) -> torch.Tensor:
+    """
+    Calculate the Jacobian more efficiently than ` torch.autograd.functional.jacobian`
+    :param encoder: the model to calculate the Jacobian of
+    :param latents: the inputs for evaluating the model
+    :param normalize: flag to rescale the Jacobianto have unit norm
+    :return: B x n_out x n_in
+    """
+
     jacob = []
     input_vars = latents.clone().requires_grad_(True)
+    encoder.eval() #otherwise we will get 0 gradients
     output_vars = encoder(input_vars)
+
     for i in range(output_vars.shape[1]):
-        jacob.append(torch.autograd.grad(output_vars[:, i:i+1], input_vars, create_graph=True, 
-                                         grad_outputs=torch.ones(output_vars[:,i:i+1].shape).to(output_vars.device))[0])
-                            
+        jacob.append(torch.autograd.grad(output_vars[:, i:i + 1], input_vars, create_graph=True,
+                                         grad_outputs=torch.ones(output_vars[:, i:i + 1].shape).to(output_vars.device))[
+                         0])
+
     jacobian = torch.stack(jacob, 1)
 
     # make the Jacobian volume preserving
     # print(jacobian.shape)
     if normalize is True:
-        jacobian *= jacobian.det().abs().pow(1/jacobian.shape[0]).reshape(-1,1,1)
+        jacobian *= jacobian.det().abs().pow(1 / jacobian.shape[0]).reshape(-1, 1, 1)
 
     return jacobian
 
-def calc_dependency_matrix(encoder:nn.Module, latents:torch.Tensor)->torch.Tensor:
+
+def calc_dependency_matrix(encoder: nn.Module, latents: torch.Tensor) -> torch.Tensor:
     """
     Calculates the dependecy matrix, which is
     :param encoder: a mapping f from the latent factors Z to the noise variables N
@@ -34,7 +45,8 @@ def calc_dependency_matrix(encoder:nn.Module, latents:torch.Tensor)->torch.Tenso
     # take the absolute value
     return jacob.abs()
 
-def sparsity_loss(dep_mat:torch.Tensor)->torch.Tensor:
+
+def sparsity_loss(dep_mat: torch.Tensor) -> torch.Tensor:
     """
     Calculates the sparsity-inducing (i.e., L1) loss for the dependency matrix.
 
@@ -43,7 +55,8 @@ def sparsity_loss(dep_mat:torch.Tensor)->torch.Tensor:
     """
     return dep_mat.abs().sum()
 
-def triangularity_loss(dep_mat:torch.Tensor)->torch.Tensor:
+
+def triangularity_loss(dep_mat: torch.Tensor) -> torch.Tensor:
     """
     Calculates the loss term for inducing a **lower** triangular structure for the dependency matrix.
     This is calculated as the L2 squared norm of the upper triangular part of the dependency matrix
@@ -55,7 +68,8 @@ def triangularity_loss(dep_mat:torch.Tensor)->torch.Tensor:
 
     return torch.triu(dep_mat, 1).pow(2).sum()
 
-def dependency_loss(dep_mat:torch.Tensor, weight_sparse:float=1., weight_triangular:float=1.)->torch.Tensor:
+
+def dependency_loss(dep_mat: torch.Tensor, weight_sparse: float = 1., weight_triangular: float = 1.) -> torch.Tensor:
     """
     Calculates the weighted sum of the triangularity-enforcing and the sparsity-inducing losses for the
     dependency matrix.
@@ -69,8 +83,7 @@ def dependency_loss(dep_mat:torch.Tensor, weight_sparse:float=1., weight_triangu
     sparse_loss = sparsity_loss(dep_mat)
     triangular_loss = triangularity_loss(dep_mat)
 
-
-    return weight_sparse*sparse_loss + weight_triangular*triangular_loss
+    return weight_sparse * sparse_loss + weight_triangular * triangular_loss
 
 
 def calc_jacobian_loss(args, f, g, latent_space):