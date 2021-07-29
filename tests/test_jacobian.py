0b94f271b672189067ecb6cf957540033190cc96
Patrik Reizinger <prmedia17@gmail.com>
2021-07-29 12:37:56 +0200

    updated flows sudmodule

--- tests/test_jacobian.py
+++ tests/test_jacobian.py
@@ -10,12 +10,13 @@ from src.prob_utils import setup_marginal, setup_conditional
 from src.utils import setup_seed, set_learning_mode, set_device
 
 
-@pytest.fixture(params=[2,3])
+@pytest.fixture(params=[2, 3])
 def args(request):
     args = argparse.Namespace(act_fct='leaky_relu', alpha=0.5, batch_size=6144, box_max=1.0, box_min=0.0, c_p=1,
                               c_param=0.05, identity_mixing_and_solution=False, inject_structure=False,
                               learnable_mask=False, load_f=None, load_g=None, lr=0.0001, m_p=0, m_param=1.0,
-                              mode='unsupervised', more_unsupervised=1, n=request.param, n_eval_samples=512, n_log_steps=250,
+                              mode='unsupervised', more_unsupervised=1, n=request.param, n_eval_samples=512,
+                              n_log_steps=250,
                               n_mixing_layer=3, n_steps=100001, no_cuda=False, normalization='', notes=None,
                               num_eval_batches=10, num_permutations=50, p=1, preserve_vol=False, project='experiment',
                               resume_training=False, save_dir='', seed=0, space_type='box', sphere_r=1.0, tau=1.0,
