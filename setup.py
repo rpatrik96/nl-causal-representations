8bf31c1f02a81b5fe4e1caedb38ae5a8d6c5da8d
Patrik Reizinger <prmedia17@gmail.com>
2021-07-30 11:31:10 +0200

    conftest modification

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup, find_packages
 setup(
     name='causalnlica',
     version='0.1.0',
-    packages=find_packages()+find_packages(where='./src')+find_packages(where='./pytorch_flows')
+    packages=find_packages(include=['pytorch_flows', 'pytorch_flows.*', 'src', 'src.*'])
 )
