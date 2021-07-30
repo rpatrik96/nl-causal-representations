13a21e9814c21842c88bcbcd35a3ec6fc4db6ee9
Patrik Reizinger <prmedia17@gmail.com>
2021-07-30 12:31:25 +0200

    update conftest

---
+++ conftest.py
@@ -0,0 +1,7 @@
+import os
+import sys
+# sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.join('', 'pytorch_flows'))))
+# sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.join('', 'cl_ica'))))
+
+print(sys.path)
+print("conftest-------------")
