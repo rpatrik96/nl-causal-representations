d9884cacf225959e9fd4f9197657853c87312542
Patrik Reizinger <preizinger@bg-slurmb-login1.novalocal>
2021-10-21 13:59:32 +0200

    package autoinstall in cl_causal

--- care_nl_ica/cl_causal.py
+++ care_nl_ica/cl_causal.py
@@ -1,26 +1,39 @@
-from args import parse_args
-from cl_ica import latent_spaces
-from indep_check import IndependenceChecker
-# from torch.utils.tensorboard import SummaryWriter
-# writer = SummaryWriter()
-from prob_utils import setup_marginal, setup_conditional, check_independence_z_gz
-from runner import Runner
-from utils import setup_seed, save_state_dict, set_learning_mode, set_device
+import os
+import pip
+
+
+def install_package():
+    """
+    Install the current package to ensure that imports work.
+    """
+
+    pip.main(["install", f"{os.path.dirname(os.path.dirname(os.path.abspath(__file__)))}", "--upgrade"])
+
 
 
 def main():
+
+    # install the package
+    install_package()
+
     # setup
+    from args import parse_args
     args = parse_args()
 
+    from prob_utils import setup_marginal, setup_conditional, check_independence_z_gz
+    from runner import Runner
+    from utils import setup_seed, save_state_dict, set_learning_mode, set_device
     set_device(args)
     setup_seed(args.seed)
     set_learning_mode(args)
 
     runner = Runner(args)
 
+    from indep_check import IndependenceChecker
     indep_checker = IndependenceChecker(args)
 
     # distributions
+    from cl_ica import latent_spaces
     latent_space = latent_spaces.LatentSpace(space=(runner.model.space), sample_marginal=(setup_marginal(args)),
                                              sample_conditional=(setup_conditional(args)), )
 
