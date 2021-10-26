8d6e2f1c195137a4721fd6dce40a6925219ebd2d
Patrik Reizinger <preizinger@slurm-bm-34.novalocal>
2021-10-26 14:11:49 +0200

    ar mlp tests

--- tests/test_jacobian.py
+++ tests/test_jacobian.py
@@ -1,4 +1,5 @@
 import argparse
+from collections import namedtuple
 
 import pytest
 import torch
@@ -10,18 +11,24 @@ from care_nl_ica.prob_utils import setup_marginal, setup_conditional
 from care_nl_ica.utils import setup_seed, set_learning_mode, set_device
 from care_nl_ica.mlp import ARBottleneckNet
 
-@pytest.fixture(params=[2, 3])
+arg_matrix = namedtuple('arg_matrix', ['n', 'use_ar_mlp'])
+
+@pytest.fixture(params=[arg_matrix(n=2, use_ar_mlp=False),
+                        arg_matrix(n=2, use_ar_mlp=True),
+                        arg_matrix(n=3, use_ar_mlp=False),
+                        arg_matrix(n=3, use_ar_mlp=True)])
 def args(request):
+
     args = argparse.Namespace(act_fct='leaky_relu', alpha=0.5, batch_size=6144, box_max=1.0, box_min=0.0, c_p=1,
                               c_param=0.05, identity_mixing_and_solution=False, inject_structure=False,
                               learnable_mask=False, load_f=None, load_g=None, lr=0.0001, m_p=0, m_param=1.0,
-                              mode='unsupervised', more_unsupervised=1, n=request.param, n_eval_samples=512,
+                              mode='unsupervised', more_unsupervised=1, n=request.param.n, n_eval_samples=512,
                               n_log_steps=250,
                               n_mixing_layer=3, n_steps=100001, no_cuda=False, normalization='', notes=None,
                               num_eval_batches=10, num_permutations=50, p=1, preserve_vol=False, project='experiment',
                               resume_training=False, save_dir='', seed=0, space_type='box', sphere_r=1.0, tau=1.0,
-                              use_batch_norm=True, use_dep_mat=True, use_flows=False, use_reverse=False,
-                              use_wandb=False, variant=1, verbose=False, use_ar_mlp=True)
+                              use_batch_norm=True, use_dep_mat=True, use_flows=not request.param.use_ar_mlp, use_reverse=False,
+                              use_wandb=False, variant=1, verbose=False, use_ar_mlp=request.param.use_ar_mlp)
 
     set_device(args)
     setup_seed(args.seed)
@@ -80,6 +87,10 @@ def test_triangularity_naive(model: ContrastiveLearningModel, network):
     y0 = model._modules[network](torch.ones(batch_size, model.hparams.n).to(model.hparams.device))
     print(f"{y0=}")
 
+    # unsqueeze for the AR MLP
+    if len(y0.shape) == 1:
+        y0 = y0.unsqueeze(0)
+
     # loop for perturbing each input one-by-one
     for i in range(model.hparams.n):
         z = torch.ones(batch_size, model.hparams.n).to(model.hparams.device)
@@ -91,6 +102,10 @@ def test_triangularity_naive(model: ContrastiveLearningModel, network):
 
         print(f"{i=},{y=}")
 
+          # unsqueeze for the AR MLP
+        if len(y.shape) == 1:
+            y = y.unsqueeze(0)
+
         # the indexing is up to the ith element
         # as input i affects outputs i:n
         # so a change before that is a failure
@@ -101,59 +116,4 @@ def test_triangularity_naive(model: ContrastiveLearningModel, network):
         model.train()
 
     assert tria_check.sum() == 0
-
-
-def main():
-    net = ARBottleneckNet(2, [1, 5, 2], [2, 3, 1])
-    mixing = torch.tensor([[2.,3],[4,5]])
-    batch_size = 10
-    x = torch.randn(batch_size, 2)
-    net(x@mixing)
-
-
-    # test jacobian
-
-
-    # calculate the Jacobian with autodiff
-    dep_mat =  calc_jacobian(net, x).abs().mean(0)
-    print(f"{dep_mat=}")
-
-    assert (torch.tril(dep_mat) != dep_mat).sum() == 0
-    
-    
-    # calculate the Jacobian with finite differences
-    # constants
-    batch_size = 1
-    tria_check = torch.zeros(net.num_vars)
-
-    # set to eval mode but remember original state
-    in_training: bool = net.training
-    net.eval()  # otherwise we will get 0 gradients
-
-    # calculate the baseline output - all inputs should be different from 0
-    # this is to avoid degenerate cases making the test succeed
-    y0 = net(torch.ones(batch_size, net.num_vars))
-    print(f"{y0=}")
-
-    # loop for perturbing each input one-by-one
-    for i in range(net.num_vars):
-        z = torch.ones(batch_size, net.num_vars)
-        z[:, i] = -1
-
-        y = net(z)
-
-        print(f"{i=},{y=}")
-
-        # the indexing is up to the ith element
-        # as input i affects outputs i:n
-        # so a change before that is a failure
-        tria_check[i] = (y[ :i] != y0[ :i]).sum()
-
-    # set back to original mode
-    if in_training is True:
-        net.train()
-
-    assert tria_check.sum() == 0
-
-
 
