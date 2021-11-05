6bc4c5e2edbbb44a8a2667ad91d28acba1c678d0
Patrik Reizinger <prmedia17@gmail.com>
2021-11-05 15:37:15 +0100

    dep mat fix

--- care_nl_ica/dep_mat.py
+++ care_nl_ica/dep_mat.py
@@ -148,7 +148,7 @@ def calc_jacobian_loss(model, latent_space, eps=1e-6, calc_numerical:bool=False)
 
     # 3/b calculate the numerical jacobian
     # calculate numerical jacobian
-    numerical_jacobian = None if calc_numerical is False else calc_jacobian_numerical(f, obs,dep_mat.shape[0], args.device, eps)
+    numerical_jacobian = None if calc_numerical is False else calc_jacobian_numerical(model.encoder, obs,dep_mat.shape[0], args.device, eps)
     # 4. calculate the loss for the dependency matrix
     dep_loss = dependency_loss(dep_mat)
     return dep_loss, dep_mat, numerical_jacobian, jac_enc_dec
