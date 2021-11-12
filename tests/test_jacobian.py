4c8cf0a564b8574c2b66044c53ea423a9668af35
Patrik Reizinger <prmedia17@gmail.com>
2021-11-12 16:33:06 +0100

    test fixture args fix

--- tests/test_jacobian.py
+++ tests/test_jacobian.py
@@ -3,7 +3,6 @@ from collections import namedtuple
 
 import pytest
 import torch
-
 from torch.autograd.functional import jacobian
 
 from care_nl_ica.cl_ica import latent_spaces
@@ -11,16 +10,15 @@ from care_nl_ica.dep_mat import calc_jacobian, calc_jacobian_numerical
 from care_nl_ica.model import ContrastiveLearningModel
 from care_nl_ica.prob_utils import setup_marginal, setup_conditional
 from care_nl_ica.utils import setup_seed, set_learning_mode, set_device
-from care_nl_ica.mlp import ARBottleneckNet
 
 arg_matrix = namedtuple('arg_matrix', ['n', 'use_ar_mlp'])
+
 
 @pytest.fixture(params=[arg_matrix(n=2, use_ar_mlp=False),
                         arg_matrix(n=2, use_ar_mlp=True),
                         arg_matrix(n=3, use_ar_mlp=False),
                         arg_matrix(n=3, use_ar_mlp=True)])
 def args(request):
-
     args = argparse.Namespace(act_fct='leaky_relu', alpha=0.5, batch_size=6144, box_max=1.0, box_min=0.0, c_p=1,
                               c_param=0.05, identity_mixing_and_solution=False, inject_structure=False,
                               learnable_mask=False, load_f=None, load_g=None, lr=0.0001, m_p=0, m_param=1.0,
@@ -29,8 +27,10 @@ def args(request):
                               n_mixing_layer=3, n_steps=100001, no_cuda=False, normalization='', notes=None,
                               num_eval_batches=10, num_permutations=50, p=1, preserve_vol=False, project='experiment',
                               resume_training=False, save_dir='', seed=0, space_type='box', sphere_r=1.0, tau=1.0,
-                              use_batch_norm=True, use_dep_mat=True, use_flows=not request.param.use_ar_mlp, use_reverse=False,
-                              use_wandb=False, variant=1, verbose=False, use_ar_mlp=request.param.use_ar_mlp, use_sem=False, use_bias=False, l1=0.0, l2=0.0)
+                              use_batch_norm=True, use_dep_mat=True, use_flows=not request.param.use_ar_mlp,
+                              use_reverse=False,
+                              use_wandb=False, variant=1, verbose=False, use_ar_mlp=request.param.use_ar_mlp,
+                              use_sem=False, use_bias=False, l1=0.0, l2=0.0, data_gen_mode='rvs')
 
     set_device(args)
     setup_seed(args.seed)
@@ -38,13 +38,15 @@ def args(request):
 
     return args
 
+
 @pytest.fixture()
 def model(args):
     return ContrastiveLearningModel(args)
 
 
 @pytest.mark.parametrize("network", ["decoder", "encoder"])
-def test_triangularity_jacobian(model: ContrastiveLearningModel, network, numerical_check:bool=False, built_in_jacobian_check:bool=False):
+def test_triangularity_jacobian(model: ContrastiveLearningModel, network, numerical_check: bool = False,
+                                built_in_jacobian_check: bool = False):
     """
 
     Checks the AR nature of the model by calculating the Jacobian.
@@ -53,7 +55,7 @@ def test_triangularity_jacobian(model: ContrastiveLearningModel, network, numeri
     :param network: model components
     :return:
     """
-    
+
     # draw a sample from the latent space
     latent_space = latent_spaces.LatentSpace(space=model.space, sample_marginal=(setup_marginal(model.hparams)),
                                              sample_conditional=(setup_conditional(model.hparams)), )
@@ -63,19 +65,17 @@ def test_triangularity_jacobian(model: ContrastiveLearningModel, network, numeri
     dep_mat = calc_jacobian(model._modules[network], z, normalize=model.hparams.preserve_vol).abs().mean(0)
     print(f"{dep_mat=}")
 
-    
     # numerical Jacobian
     if numerical_check is True:
         print(f"{calc_jacobian_numerical(model._modules[network], z, model.hparams.n, model.hparams.device)=}")
 
     # same as calc_jacobian, but using the torch jacobian function
     if built_in_jacobian_check is True:
-
         # x in shape (Batch, Length)
         def _func_sum(x):
             return model._modules[network].forward(x).sum(dim=0)
 
-        print(jacobian(_func_sum, z).permute(1,0,2).abs().mean(0))
+        print(jacobian(_func_sum, z).permute(1, 0, 2).abs().mean(0))
 
     assert (torch.tril(dep_mat) != dep_mat).sum() == 0
 
@@ -111,12 +111,12 @@ def test_triangularity_naive(model: ContrastiveLearningModel, network):
     for i in range(model.hparams.n):
         z = torch.ones(batch_size, model.hparams.n).to(model.hparams.device)
         z[:, i] = -1
-        
+
         y = model._modules[network](z)
 
         print(f"{i=},{y=}")
 
-          # unsqueeze for the AR MLP
+        # unsqueeze for the AR MLP
         if len(y.shape) == 1:
             y = y.unsqueeze(0)
 
@@ -130,4 +130,3 @@ def test_triangularity_naive(model: ContrastiveLearningModel, network):
         model.train()
 
     assert tria_check.sum() == 0
-
