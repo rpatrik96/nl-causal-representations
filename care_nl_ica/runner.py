entropy sign fix

care_nl_ica/runner.py
@@ -234,7 +234,7 @@ class Runner(object):
                 probs = torch.nn.functional.softmax(self.model.encoder.sinkhorn.doubly_stochastic_matrix, -1).view(
                     -1, )
 
-                total_loss_value -= self.hparams.entropy_coeff * torch.distributions.Categorical(probs).entropy()
+                total_loss_value += self.hparams.entropy_coeff * torch.distributions.Categorical(probs).entropy()
 
             if self.dep_loss is not None:
                 total_loss_value += self.dep_loss