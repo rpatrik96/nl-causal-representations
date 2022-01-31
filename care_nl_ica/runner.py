ebf3d4ea1a21309bc7fa7fa5b97aa947c77ce511
Patrik Reizinger <prmedia17@gmail.com>
2022-01-31 07:56:51 +0100

    losses refactor

--- care_nl_ica/runner.py
+++ care_nl_ica/runner.py
@@ -12,7 +12,7 @@ from cl_ica import latent_spaces
 from dep_mat import calc_jacobian_loss
 from indep_check import IndependenceChecker
 from metric_logger import Metrics
-from prob_utils import setup_marginal, setup_conditional, frobenius_diagonality
+from prob_utils import setup_marginal, setup_conditional
 
 
 class Runner(object):
@@ -149,28 +149,11 @@ class Runner(object):
         self.optimizer = torch.optim.Adam(self.model.encoder.parameters(), lr=self.hparams.lr)
 
     def train_step(self, data, h, test):
-        device = self.hparams.device
-
-        n1, n2_con_n1, n3 = data
-        n1 = n1.to(device)
-        n2_con_n1 = n2_con_n1.to(device)
-        n3 = n3.to(device)
-
-        # create random "negative" pairs
-        # this is faster than sampling n3 again from the marginal distribution
-        # and should also yield samples as if they were sampled from the marginal
-        # import pdb; pdb.set_trace()
-        # n3_shuffle_indices = torch.randperm(len(n1))
-        # n3_shuffle_indices = torch.roll(torch.arange(len(n1)), 1)
-        # n3 = n1[n3_shuffle_indices]
-        # n3 = n3.to(device)
+        n1, n2_con_n1, n3 = self._prepare_data(data)
 
         self.optimizer.zero_grad()
 
-        n1_rec = h(n1)
-        n2_con_n1_rec = h(n2_con_n1)
-        n3_rec = h(n3)
-        # n3_rec = n1_rec[n3_shuffle_indices]
+        n1_rec, n2_con_n1_rec, n3_rec = self._forward(h, n1, n2_con_n1, n3)
 
         self.logger.log_scatter_latent_rec(n1, n1_rec, "n1")
 
@@ -178,6 +161,34 @@ class Runner(object):
             z1 = self.model.decoder(n1)
             self.logger.log_scatter_latent_rec(z1, n1_rec, "z1_n1_rec")
 
+        losses_value, total_loss_value = self._contrastive_loss(n1, n1_rec, n2_con_n1, n2_con_n1_rec, n3, n3_rec, test)
+
+        if not self.hparams.identity_mixing_and_solution and self.hparams.lr != 0:
+            # add the learnable jacobian
+            total_loss_value = self._learnable_jacobian_loss(n1, n1_rec, n2_con_n1, n2_con_n1_rec, n3, n3_rec,
+                                                             total_loss_value)
+            total_loss_value = self._l2_loss(total_loss_value)
+            total_loss_value = self._l1_loss(total_loss_value)
+            total_loss_value = self._sinkhorn_entropy_loss(total_loss_value)
+            total_loss_value = self._dep_loss(total_loss_value)
+            total_loss_value = self._triangularity_loss(n1, n1_rec, n2_con_n1, n2_con_n1_rec, n3, n3_rec,
+                                                        total_loss_value)
+            total_loss_value = self._qr_loss(total_loss_value)
+
+            total_loss_value.backward()
+
+            self.optimizer.step()
+
+        return total_loss_value.item(), unpack_item_list(losses_value)
+
+    def _forward(self, h, n1, n2_con_n1, n3):
+        n1_rec = h(n1)
+        n2_con_n1_rec = h(n2_con_n1)
+        n3_rec = h(n3)
+        # n3_rec = n1_rec[n3_shuffle_indices]
+        return n1_rec, n2_con_n1_rec, n3_rec
+
+    def _contrastive_loss(self, n1, n1_rec, n2_con_n1, n2_con_n1_rec, n3, n3_rec, test):
         if test:
             total_loss_value = F.mse_loss(n1_rec, n1)
             losses_value = [total_loss_value]
@@ -191,116 +202,143 @@ class Runner(object):
             #    n1, n2_con_n1, n3, n1, n2_con_n1, n3
             # )[0], global_step)
             # writer.flush()
+        return losses_value, total_loss_value
 
-        if not self.hparams.identity_mixing_and_solution and self.hparams.lr != 0:
+    def _prepare_data(self, data):
+        n1, n2_con_n1, n3 = data
+        n1 = n1.to(self.hparams.device)
+        n2_con_n1 = n2_con_n1.to(self.hparams.device)
+        n3 = n3.to(self.hparams.device)
+        # create random "negative" pairs
+        # this is faster than sampling n3 again from the marginal distribution
+        # and should also yield samples as if they were sampled from the marginal
+        # import pdb; pdb.set_trace()
+        # n3_shuffle_indices = torch.randperm(len(n1))
+        # n3_shuffle_indices = torch.roll(torch.arange(len(n1)), 1)
+        # n3 = n1[n3_shuffle_indices]
+        # n3 = n3.to(device)
+        return n1, n2_con_n1, n3
 
-            # add the learnable jacobian
-            if self.hparams.learn_jacobian is True:
-                # get the data after mixing (decoding)
-                z1 = self.model.decoder(n1)
-                z2_con_z1 = self.model.decoder(n2_con_n1)
-                z3 = self.model.decoder(n3)
+    def _qr_loss(self, total_loss_value):
+        if self.hparams.start_step is None or (
+                self.hparams.start_step is not None and self.logger.global_step >= self.hparams.start_step):
 
-                # reconstruct linearly
-                n1_tilde = self.model.jacob(z1)
-                n2_con_n1_tilde = self.model.jacob(z2_con_z1)
-                n3_tilde = self.model.jacob(z3)
+            Q = self.model.encoder.ar_bottleneck.assembled_weight.T.qr()[0]
 
-                eps = 1e-8
+            """
+            The first step is to ensure that the Q in the QR decomposition of the transposed(bottleneck) is 
+            **a permutation** matrix.
 
-                # standardize n
-                n1_rec_std = ((n1_rec - n1_rec.mean(dim=0)) / (n1_rec.std(dim=0) + eps) + n1_rec.mean(dim=0)).detach()
-                n2_con_n1_rec_std = ((n2_con_n1_rec - n2_con_n1_rec.mean(dim=0)) / (
-                        n2_con_n1_rec.std(dim=0) + eps) + n2_con_n1_rec.mean(dim=0)).detach()
-                n3_rec_std = ((n3_rec - n3_rec.mean(dim=0)) / (n3_rec.std(dim=0) + eps) + n3_rec.mean(dim=0)).detach()
+            The second step is to ensure that the permutation matrix is the identity. If we got a permutation matrix
+            in the first step, then we could use Q.T to multiply the observations. 
+            """
 
-                lin_mse = (n1_tilde - n1_rec_std).pow(2).mean() + (n2_con_n1_tilde - n2_con_n1_rec_std).pow(
-                    2).mean() + (n3_tilde - n3_rec_std).pow(2).mean()
+            # loss options
+            if self.logger.global_step % 250 == 0:
+                print(f"{Q=}")
 
-                total_loss_value += lin_mse
+            # 1. diagonality (as Q^n = I for permutation matrices)
+            # total_loss_value+=self.hparams.qr_loss*frobenius_diagonality(Q.matrix_power(Q.shape[0]).abs())
 
-            if self.hparams.l2 != 0.0:
-                l2: float = 0.0
-                for param in self.model.encoder.parameters():
-                    l2 += torch.sum(param ** 2)
+            # 2. rows and cols sum up to 1
+            col_sum = Q.abs().sum(0)
+            row_sum = Q.abs().sum(1)
 
-                total_loss_value += self.hparams.l2 * l2
+            total_loss_value += self.hparams.qr_loss * ((col_sum - torch.ones_like(col_sum)).abs().mean() + (
+                    row_sum - torch.ones_like(row_sum)).abs().mean())
 
-            if self.hparams.l1 != 0 and self.hparams.use_ar_mlp is True:
-                # add sparsity loss to the AR MLP bottleneck
-                total_loss_value += self.hparams.l1 * self.model.encoder.bottleneck_l1_norm
+        return total_loss_value
 
-            if self.hparams.permute is True:
+    def _learnable_jacobian_loss(self, n1, n1_rec, n2_con_n1, n2_con_n1_rec, n3, n3_rec, total_loss_value):
+        if self.hparams.learn_jacobian is True:
+            # get the data after mixing (decoding)
+            z1 = self.model.decoder(n1)
+            z2_con_z1 = self.model.decoder(n2_con_n1)
+            z3 = self.model.decoder(n3)
 
-                probs = torch.nn.functional.softmax(self.model.sinkhorn.doubly_stochastic_matrix, -1).view(
-                    -1, )
+            # reconstruct linearly
+            n1_tilde = self.model.jacob(z1)
+            n2_con_n1_tilde = self.model.jacob(z2_con_z1)
+            n3_tilde = self.model.jacob(z3)
 
-                total_loss_value += self.hparams.entropy_coeff * torch.distributions.Categorical(probs).entropy()
+            eps = 1e-8
 
-            if self.dep_loss is not None:
-                total_loss_value += self.dep_loss
+            # standardize n
+            n1_rec_std = ((n1_rec - n1_rec.mean(dim=0)) / (n1_rec.std(dim=0) + eps) + n1_rec.mean(dim=0)).detach()
+            n2_con_n1_rec_std = ((n2_con_n1_rec - n2_con_n1_rec.mean(dim=0)) / (
+                    n2_con_n1_rec.std(dim=0) + eps) + n2_con_n1_rec.mean(dim=0)).detach()
+            n3_rec_std = ((n3_rec - n3_rec.mean(dim=0)) / (n3_rec.std(dim=0) + eps) + n3_rec.mean(dim=0)).detach()
 
-            if self.hparams.triangularity_loss != 0.:
-                from prob_utils import corr_matrix
-                from dep_mat import triangularity_loss
+            lin_mse = (n1_tilde - n1_rec_std).pow(2).mean() + (n2_con_n1_tilde - n2_con_n1_rec_std).pow(
+                2).mean() + (n3_tilde - n3_rec_std).pow(2).mean()
 
-                # todo: these use the ground truth
-                # still, they can be used to show that some supervision helps
-                # pearson_n1 = corr_matrix(n1.T, n1_rec.T)
-                # pearson_n2_con_n1 = corr_matrix(n2_con_n1.T, n2_con_n1_rec.T)
-                # pearson_n3 = corr_matrix(n3.T, n3_rec.T)
-                # total_loss_value += self.hparams.triangularity_loss*(frobenius_diagonality(pearson_n1.abs()) + frobenius_diagonality(
-                #     pearson_n2_con_n1.abs()) + frobenius_diagonality(pearson_n3.abs()))
+            total_loss_value += lin_mse
+        return total_loss_value
 
-                # correlation between observation and reconstructed latents
-                # exploits the assumption that the SEM has a lower-triangular Jacobian
-                # order is important due to the triangularity loss
-                pearson_n1 = corr_matrix( self.model.decoder(n1).T, n1_rec.T)
-                pearson_n2_con_n1 = corr_matrix(self.model.decoder(n2_con_n1).T, n2_con_n1_rec.T)
-                pearson_n3 = corr_matrix(self.model.decoder(n3).T, n3_rec.T)
-                total_loss_value += self.hparams.triangularity_loss * (
-                            triangularity_loss(pearson_n1) + triangularity_loss(
-                        pearson_n2_con_n1) + triangularity_loss(pearson_n3))
+    def _triangularity_loss(self, n1, n1_rec, n2_con_n1, n2_con_n1_rec, n3, n3_rec, total_loss_value):
+        if self.hparams.triangularity_loss != 0.:
+            from prob_utils import corr_matrix
+            from dep_mat import triangularity_loss
 
-                # cos_sim = torch.nn.CosineSimilarity(dim=1, eps=1e-6)
-                # pearson_n1 = cos_sim(n1 - n1.mean(dim=1, keepdim=True), n1_rec - n1_rec.mean(dim=1, keepdim=True))
-                # pearson_n2_con_n1 = cos_sim(n2_con_n1 - n2_con_n1.mean(dim=1, keepdim=True), n2_con_n1_rec - n2_con_n1_rec.mean(dim=1, keepdim=True))
-                # pearson_n3 = cos_sim(n3 - n3.mean(dim=1, keepdim=True), n3_rec - n3_rec.mean(dim=1, keepdim=True))
+            # todo: these use the ground truth
+            # still, they can be used to show that some supervision helps
+            # pearson_n1 = corr_matrix(n1.T, n1_rec.T)
+            # pearson_n2_con_n1 = corr_matrix(n2_con_n1.T, n2_con_n1_rec.T)
+            # pearson_n3 = corr_matrix(n3.T, n3_rec.T)
+            # total_loss_value += self.hparams.triangularity_loss*(frobenius_diagonality(pearson_n1.abs()) + frobenius_diagonality(
+            #     pearson_n2_con_n1.abs()) + frobenius_diagonality(pearson_n3.abs()))
 
+            # correlation between observation and reconstructed latents
+            # exploits the assumption that the SEM has a lower-triangular Jacobian
+            # order is important due to the triangularity loss
+            pearson_n1 = corr_matrix(self.model.decoder(n1).T, n1_rec.T)
+            pearson_n2_con_n1 = corr_matrix(self.model.decoder(n2_con_n1).T, n2_con_n1_rec.T)
+            pearson_n3 = corr_matrix(self.model.decoder(n3).T, n3_rec.T)
+            total_loss_value += self.hparams.triangularity_loss * (
+                    triangularity_loss(pearson_n1) + triangularity_loss(
+                pearson_n2_con_n1) + triangularity_loss(pearson_n3))
+
+            """
+            Problems: both Sinkhorn and QR degenerates to the identity
             
-            if self.hparams.qr_loss != 0. and self.hparams.use_ar_mlp is True:
+            What we could do: 
+                1. calculate the correlation matrice above
+                2. DETACH
+                3. deploy Sinkhorn to minimize the triangularity loss
+            -> this would separate ICA + learning the ordering
+            
+            
+            """
 
+        return total_loss_value
 
-                if self.hparams.start_step is None or (self.hparams.start_step is not None and self.logger.global_step >= self.hparams.start_step):
+    def _dep_loss(self, total_loss_value):
+        if self.dep_loss is not None:
+            total_loss_value += self.dep_loss
+        return total_loss_value
 
-                    Q = self.model.encoder.ar_bottleneck.assembled_weight.T.qr()[0]
+    def _sinkhorn_entropy_loss(self, total_loss_value):
+        if self.hparams.entropy_coeff != 0. and self.hparams.permute is True:
+            probs = torch.nn.functional.softmax(self.model.sinkhorn.doubly_stochastic_matrix, -1).view(
+                -1, )
 
-                    """
-                    The first step is to ensure that the Q in the QR decomposition of the transposed(bottleneck) is 
-                    **a permutation** matrix.
-                    
-                    The second step is to ensure that the permutation matrix is the identity. If we got a permutation matrix
-                    in the first step, then we could use Q.T to multiply the observations. 
-                    """
+            total_loss_value += self.hparams.entropy_coeff * torch.distributions.Categorical(probs).entropy()
+        return total_loss_value
 
-                    # loss options
+    def _l1_loss(self, total_loss_value):
+        if self.hparams.l1 != 0 and self.hparams.use_ar_mlp is True:
+            # add sparsity loss to the AR MLP bottleneck
+            total_loss_value += self.hparams.l1 * self.model.encoder.bottleneck_l1_norm
+        return total_loss_value
 
-                    # 1. diagonality (as Q^n = I for permutation matrices)
-                    total_loss_value+=frobenius_diagonality(Q.matrix_power(Q.shape[0]).abs())
+    def _l2_loss(self, total_loss_value):
+        if self.hparams.l2 != 0.0:
+            l2: float = 0.0
+            for param in self.model.encoder.parameters():
+                l2 += torch.sum(param ** 2)
 
-                    # 2. rows and cols sum up to 1
-                    # col_sum = Q.sum(0)
-                    # row_sum = Q.sum(1)
-
-                    # (col_sum - torch.ones_like(col_sum)).norm(p=2)
-                    # (row_sum - torch.ones_like(row_sum)).norm(p=2)
-
-
-            total_loss_value.backward()
-
-            self.optimizer.step()
-
-        return total_loss_value.item(), unpack_item_list(losses_value)
+            total_loss_value += self.hparams.l2 * l2
+        return total_loss_value
 
     def train(self, data, h, test):
         if self.hparams.lr != 0:
@@ -319,14 +357,12 @@ class Runner(object):
 
             while (self.logger.global_step <= self.hparams.n_steps if learning_mode else self.logger.global_step <= (
                     self.hparams.n_steps * self.hparams.more_unsupervised)):
-
                 # if self.logger.global_step > 7000:
                 #
                 #     for p in self.model.parameters():
                 #         p.requires_grad = False
                 #
                 #     self.model.sinkhorn.weight.requires_grad = True
-
 
                 data = sample_marginal_and_conditional(self.latent_space, size=self.hparams.batch_size,
                                                        device=self.hparams.device)
