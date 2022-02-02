3968e91e9fae708fd2e7d401eb2dde5213246249
Patrik Reizinger <preizinger@bg-slurmb-login1.novalocal>
2022-02-02 13:31:53 +0100

    steps removed from preemptable job

--- scripts/start_preemptable_job.sh
+++ scripts/start_preemptable_job.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
 PARTITION=gpu-2080ti-preemptable
 #FLAGS= 
-PYTHONPATH=. srun --job-name="$JOB_NAME" --partition=$PARTITION --cpus-per-task=2 --mem=8G --pty --gres=gpu:1 -- /mnt/qb/work/bethge/preizinger/nl-causal-representations//scripts/run_singularity_server.sh python3 /mnt/qb/work/bethge/preizinger/nl-causal-representations/care_nl_ica/cl_causal.py  --project mlp-test --use-batch-norm --use-ar-mlp --use-dep-mat --use-wandb  --n-steps 150001 "$@"
+PYTHONPATH=. srun --job-name="$JOB_NAME" --partition=$PARTITION --cpus-per-task=2 --mem=8G --pty --gpus=1 -- /mnt/qb/work/bethge/preizinger/nl-causal-representations//scripts/run_singularity_server.sh python3 /mnt/qb/work/bethge/preizinger/nl-causal-representations/care_nl_ica/cl_causal.py  --project mlp-test --use-batch-norm  --use-dep-mat --use-wandb  "$@"
 
