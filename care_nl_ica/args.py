ca2b63bae3b26ff71477ae011d302237bfbcddcf
Patrik Reizinger <prmedia17@gmail.com>
2022-02-03 17:45:04 +0100

    permutation from dep_mat

--- care_nl_ica/args.py
+++ care_nl_ica/args.py
@@ -15,6 +15,7 @@ def parse_args():
     parser.add_argument('--triangular', action='store_true', help="Force the AR MLP bottleneck to be triangular")
     parser.add_argument("--triangularity-loss", type=float, default=0.0, help="triangularity loss on the correlation matrix")
     parser.add_argument("--qr-loss", type=float, default=0.0, help="QR loss on the bottleneck matrix")
+    parser.add_argument('--cholesky-permutation', action='store_true', help="Estimate the permutation matrix from the Cholesky decomposition of the Jacobian")
     parser.add_argument('--use-sem', action='store_true', help="Use SEM as decoder")
     parser.add_argument('--sinkhorn', action='store_true', help="Use the Sinkhorn network")
     parser.add_argument('--permute', action='store_true', help="Learn the permutation")
