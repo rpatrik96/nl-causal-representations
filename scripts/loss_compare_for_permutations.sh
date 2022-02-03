db2a1315298734b415f2bf503b9483fdce6e9bf2
Patrik Reizinger <preizinger@bg-slurmb-login1.novalocal>
2022-02-03 08:49:42 +0100

    script refactor

--- scripts/loss_compare_for_permutations.sh
+++ scripts/loss_compare_for_permutations.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+N_STEPS=25001
+START_STEP=2750
+
+QR_LOSS=0
+TRIANGULARITY_LOSS=0
+ENTROPY_COEFF=1e-1
+LOSS_FLAGS="--qr-loss ${QR_LOSS} --triangularity-loss ${TRIANGULARITY_LOSS} --entropy-coeff ${ENTROPY_COEFF}"
+
+NOTE="permuted + fixed + MLP + qr"
+DATA_FLAGS="--use-sem --permute"
+LOG_FLAGS="--verbose --normalize-latents"
+MODEL_FLAGS="--use-ar-mlp"
+
 for seed in 84645646; do
   for n in 3; do
     fact=1
@@ -8,8 +21,8 @@ for seed in 84645646; do
     done
 
     for ((variant = 0; variant < ${fact}; variant++)); do
-      ./scripts/start_preemptable_job.sh --use-ar-mlp --seed ${seed} --n ${n} --variant ${variant} --note "Loss comparison with permuted data and fixed ordering" --tags normalization linear sem residual permute --use-sem --normalize-latents --verbose --permute &
-      sleep 20
+      ./scripts/start_preemptable_job.sh  --seed ${seed} --n ${n} --variant "${variant}" --note "${NOTE}"  "${LOSS_FLAGS}" --n-steps "${N_STEPS}" --start-step "${START_STEP}" "${DATA_FLAGS}" "${LOG_FLAGS}" "${MODEL_FLAGS}" &
+      sleep 5
     done
   done
 done
--- scripts/loss_compare_for_permutations_with_sinkhorn.sh
+++ scripts/loss_compare_for_permutations_with_sinkhorn.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+N_STEPS=25001
+START_STEP=2750
+
+QR_LOSS=5e-1
+TRIANGULARITY_LOSS=0
+ENTROPY_COEFF=3e-3
+LOSS_FLAGS="--qr-loss ${QR_LOSS} --triangularity-loss ${TRIANGULARITY_LOSS} --entropy-coeff ${ENTROPY_COEFF}"
+
+NOTE="permute + small S + ar bottleneck + uniform"
+DATA_FLAGS="--use-sem --permute"
+LOG_FLAGS="--verbose --normalize-latents"
+MODEL_FLAGS="--use-ar-mlp --sinkhorn"
+
 for seed in 84645646; do
   for n in 3; do
     fact=1
@@ -8,8 +21,8 @@ for seed in 84645646; do
     done
 
     for ((variant = 0; variant < ${fact}; variant++)); do
-      ./scripts/start_preemptable_job.sh --use-ar-mlp --seed ${seed} --n ${n} --variant ${variant} --note "Loss comparison with permuted data and sinkhorn + triu corr loss " --tags normalization nonlinear sem residual permute sinkhorn triangularity --use-sem --nonlin-sem --normalize-latents --verbose --permute --sinkhorn --triangularity-loss 1 &
-      sleep 20
+      ./scripts/start_preemptable_job.sh  --seed ${seed} --n ${n} --variant "${variant}" --note "${NOTE}"  ${LOSS_FLAGS} --n-steps ${N_STEPS} --start-step ${START_STEP} ${DATA_FLAGS} ${LOG_FLAGS} ${MODEL_FLAGS} "$@" &
+      sleep 5
     done
   done
 done
