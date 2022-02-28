945d5d16c9f050b2a6626bfb9e84a7a56eb4ebd7
Patrik Reizinger <prmedia17@gmail.com>
2022-02-28 16:19:09 +0100

    sh, configs

--- scripts/jacobian_normalization.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-for cparam in 0.05 0.01 0.1
-do
-    for tau in 0.5 1.0 2.0
-    do
-    ./scripts/start_preemptable_job.sh --c-param ${cparam} --tau ${tau} --note "jacobian normalization with marginal assumption" --tags nonlinear sem normalization --use-sem --nonlin-sem &
-    sleep 10
-    done
-done
--- scripts/seed_ablation_jacobian_scale_nonlin.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for seed in 0 42 7676 8778675
-do
-    ./start_preemptable_job.sh --seed ${seed} --note "jacobian scale seed test nonlinear sem" --tags nonlinear sem --use-sem --nonlin-sem &
-    sleep 10
-done
