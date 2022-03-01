ef5f344b78688cdc60b990ff90f9fa3a0f4d85e0
Patrik Reizinger <preizinger@bg-slurmb-login1.novalocal>
2022-03-01 08:46:37 +0100

    sweep start from cli

--- scripts/wandb_sweep.sh
+++ scripts/wandb_sweep.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
 PARTITION=gpu-2080ti-preemptable
 #FLAGS= 
-PYTHONPATH=. srun --job-name="$JOB_NAME" --partition=$PARTITION --cpus-per-task=4 --mem=8G --pty --gpus=1 -- /mnt/qb/work/bethge/preizinger/nl-causal-representations//scripts/run_singularity_server.sh wandb agent --count 1 causal-representation-learning/nl-causal-representations/q3yh8bx6  "$@"
+PYTHONPATH=. srun --job-name="$JOB_NAME" --partition=$PARTITION --cpus-per-task=4 --mem=8G --pty --gpus=1 -- /mnt/qb/work/bethge/preizinger/nl-causal-representations//scripts/run_singularity_server.sh wandb agent --count 1 "$@"
 
