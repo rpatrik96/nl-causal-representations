358fbb0a07bf65f867560eb8c107421fae2d86a5
Patrik Reizinger <prmedia17@gmail.com>
2022-04-21 21:00:02 +0200

    qr norm->mean

--- care_nl_ica/losses/dep_mat.py
+++ care_nl_ica/losses/dep_mat.py
@@ -50,9 +50,9 @@ def permutation_loss(matrix: torch.Tensor, matrix_power: bool = False):
         # rows and cols sum up to 1
         col_sum = matrix.abs().sum(0)
         row_sum = matrix.abs().sum(1)
-        loss = (col_sum - torch.ones_like(col_sum)).norm() + (
+        loss = (col_sum - torch.ones_like(col_sum)).pow(2).mean() + (
             row_sum - torch.ones_like(row_sum)
-        ).norm()
+        ).pow(2).mean()
     else:
         # diagonality (as Q^n = I for permutation matrices)
         loss = frobenius_diagonality(matrix.matrix_power(matrix.shape[0]).abs())
