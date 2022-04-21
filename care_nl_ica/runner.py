eae867aa2d5075f34f808910899a5f41df4b6ed8
Patrik Reizinger <prmedia17@gmail.com>
2022-04-21 07:04:19 +0200

    black

--- care_nl_ica/runner.py
+++ care_nl_ica/runner.py
@@ -150,7 +150,9 @@ class ContrastiveICAModule(pl.LightningModule):
             if (
                 correct_order := torch.all(
                     self.hard_permutation
-                    @ self.trainer.datamodule.mixing.permutation_matrix.to(self.hard_permutation.device)
+                    @ self.trainer.datamodule.mixing.permutation_matrix.to(
+                        self.hard_permutation.device
+                    )
                     == torch.eye(
                         self.hparams.latent_dim, device=self.hard_permutation.device
                     )
