602246d8baa2797340b7a06d007215dffd98ed85
Patrik Reizinger <prmedia17@gmail.com>
2022-06-15 07:55:06 +0200

    learning_stats -> analysis.py

--- notebooks/analysis.py
+++ notebooks/analysis.py
@@ -2,6 +2,7 @@ from os.path import isfile
 
 import numpy as np
 import pandas as pd
+from care_nl_ica.models.sinkhorn import learn_permutation
 
 BLUE = "#1A85FF"
 RED = "#D0021B"
@@ -189,3 +190,61 @@ def violin_by_prior(
     plt.xticks(xticks)
     # plt.tight_layout()
     plt.savefig(filename)
+
+
+def learning_stats(
+    df,
+    true_unmix_jacobians,
+    est_unmix_jacobians,
+    permute_indices,
+    hamming_threshold=1e-2,
+    selector_col="nonlin_sem",
+    weight_threshold=None,
+    dag_permute=True,
+    num_steps=5000,
+):
+
+    for dim in df.dim.unique():
+        for selector in df[selector_col].unique():
+            success = []
+            hamming = []
+            accuracy = []
+            for (selector_item, j_gt, j_est, permute) in zip(
+                df[selector_col],
+                true_unmix_jacobians,
+                est_unmix_jacobians,
+                permute_indices,
+            ):
+                if j_gt.shape[0] == dim and selector_item == selector:
+                    s, h, a = learn_permutation(
+                        j_gt,
+                        j_est,
+                        permute,
+                        triu_weigth=20.0,
+                        tril_weight=10.0,
+                        diag_weight=6.0,
+                        num_steps=num_steps,
+                        lr=1e-4,
+                        verbose=True,
+                        drop_smallest=True,
+                        threshold=weight_threshold,
+                        binary=True,
+                        hamming_threshold=hamming_threshold,
+                        dag_permute=dag_permute,
+                    )
+
+                    success.append(s)
+                    hamming.append(h)
+                    accuracy.append(a)
+
+            mcc = df.mcc[(df.dim == dim) & (df[selector_col] == selector)]
+            print("----------------------------------")
+            print("----------------------------------")
+            if len(success) > 0:
+                print(
+                    f"{dim=} ({selector_col}={selector})\tMCC={mcc.mean():.3f}+{mcc.std():.3f}\tAcc(order):{np.array(success).mean():.3f}\t  Acc:{np.array(accuracy).mean():.3f}\tSHD:{np.array(hamming).mean():.6f}\t[{len(success)} items]"
+                )
+            else:
+                print(f"No experiments for {dim=} ({selector_col}={selector})")
+            print("----------------------------------")
+            print("----------------------------------")
