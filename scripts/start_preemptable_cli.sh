861c3785ff5569d3823febc2e014ec823efc5439
Patrik Reizinger <preizinger@bg-slurmb-login1.novalocal>
2022-08-11 08:14:18 +0200

    script updates

--- scripts/start_preemptable_cli.sh
+++ scripts/start_preemptable_cli.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
 PARTITION=gpu-2080ti-preemptable
 #FLAGS= 
-PYTHONPATH=. srun --time=60 --job-name="$JOB_NAME" --partition=$PARTITION --mem=8G --pty --gpus=1 -- scripts/run_singularity_server.sh python3 care_nl_ica/cli.py fit --config configs/config.yaml  --model.weight_init_fn=orthogonal --trainer.max_epochs=1 --data.latent_dim=5 --model.start_step=400 --model.gain=1 --model.qr=0.0 --model.offline=true --data.variant=4 --data.force_chain=true --data.nonlin_sem=true  --data.use_sem=true --data.permute=false --model.use_ar_mlp=false --data.offset=1 "$@"
+PYTHONPATH=. srun --time=360 --job-name="$JOB_NAME" --partition=$PARTITION --mem=8G --pty --gpus=1 -- scripts/run_singularity_server.sh python3 care_nl_ica/cli.py fit --config configs/config.yaml  --trainer.max_epochs=25000 --data.latent_dim=10 --model.offline=true --data.variant=4 --data.force_chain=true --data.nonlin_sem=true  --data.use_sem=false --data.permute=false --model.use_ar_mlp=false --data.data_gen_mode=rvs "$@"
 
