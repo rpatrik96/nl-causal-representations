eeb8a11610224b3c70f4038a00b5de7e144c76cd
Patrik Reizinger <prmedia17@gmail.com>
2022-09-13 12:11:08 +0200

    test_runner imports

--- tests/test_runner.py
+++ tests/test_runner.py
@@ -1,6 +1,7 @@
 from pytorch_lightning.trainer import Trainer
-from care_nl_ica.runner import ContrastiveICAModule
+
 from care_nl_ica.data.datamodules import ContrastiveDataModule
+from care_nl_ica.runner import ContrastiveICAModule
 
 
 def test_runner():
