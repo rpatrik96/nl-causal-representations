7d542e53e562c2e27a8e1591e31e3c9e766044d4
Patrik Reizinger <prmedia17@gmail.com>
2022-09-23 09:27:56 +0200

    hsic correction

--- notebooks/analysis.py
+++ notebooks/analysis.py
@@ -17,6 +17,40 @@ BLUE = "#1A85FF"
 RED = "#D0021B"
 METRIC_EPS = 1e-6
 
+from matplotlib import pyplot as plt
+from matplotlib import rc
+
+
+def plot_typography(
+    usetex: bool = False, small: int = 16, medium: int = 20, big: int = 22
+):
+    """
+    Initializes font settings and visualization backend (LaTeX or standard matplotlib).
+    :param usetex: flag to indicate the usage of LaTeX (needs LaTeX indstalled)
+    :param small: small font size in pt (for legends and axes' ticks)
+    :param medium: medium font size in pt (for axes' labels)
+    :param big: big font size in pt (for titles)
+    :return:
+    """
+
+    # font family
+    rc("font", **{"family": "sans-serif", "sans-serif": ["Helvetica"]})
+    ## for Palatino and other serif fonts use:
+    # rc('font',**{'family':'serif','serif':['Palatino']})
+
+    # backend
+    rc("text", usetex=usetex)
+    rc("font", family="serif")
+
+    # font sizes
+    rc("font", size=small)  # controls default text sizes
+    rc("axes", titlesize=big)  # fontsize of the axes title
+    rc("axes", labelsize=medium)  # fontsize of the x and y labels
+    rc("xtick", labelsize=small)  # fontsize of the tick labels
+    rc("ytick", labelsize=small)  # fontsize of the tick labels
+    rc("legend", fontsize=small)  # legend fontsize
+    rc("figure", titlesize=big)  # fontsize of the figure title
+
 
 def sweep2df(
     sweep_runs,
@@ -375,18 +409,16 @@ def corrected_jacobian_stats(
 
                     if hsic is not None:
                         hsic = torch.from_numpy(hsic.astype(np.float32))
-                        hsic_edges = hsic2edge(hsic)
-                        hsic_edges_corr = correct_jacobian_permutations(
-                            hsic_edges, munkres, p
-                        )
+                        hsic_corr = correct_jacobian_permutations(hsic, munkres, p)
+                        hsic_edges = hsic2edge(hsic_corr)
 
                         numel = dim**2
-                        hsic_tp = (hsic_edges_corr == j_gt.bool()).float().sum()
+                        hsic_tp = (hsic_edges == j_gt.bool()).float().sum()
                         hsic_fp = (
-                            (hsic_edges_corr == j_gt.bool().logical_not()).float().sum()
+                            (hsic_edges == j_gt.bool().logical_not()).float().sum()
                         )
                         hsic_fn = (
-                            (hsic_edges_corr.logical_not() == j_gt.bool()).float().sum()
+                            (hsic_edges.logical_not() == j_gt.bool()).float().sum()
                         )
 
                         precision_hsic.append(
@@ -418,6 +450,8 @@ def corrected_jacobian_stats(
                 "precisions": precisions,
                 "recalls": recalls,
                 "thresholds": thresholds,
+                "accuracy": accuracy,
+                "accuracy_hsic": accuracy_hsic,
             }
     return stats
 
