6167ee32cb48451f5e7d38c7d9107e6afec1dfdb
Patrik Reizinger <prmedia17@gmail.com>
2022-09-24 17:46:03 +0200

    hinton -> analysis

--- notebooks/analysis.py
+++ notebooks/analysis.py
@@ -470,3 +470,31 @@ def hsic2edge(hsic):
 
     # due to iteration pattern, we need to transpose hsic_edges
     return hsic_edges.T
+
+
+# Source: https://matplotlib.org/3.1.1/gallery/specialty_plots/hinton_demo.html
+
+
+def hinton(matrix, max_weight=None, ax=None, filename=None):
+    """Draw Hinton diagram for visualizing a weight matrix."""
+    ax = ax if ax is not None else plt.gca()
+
+    if not max_weight:
+        max_weight = 2 ** np.ceil(np.log(np.abs(matrix).max()) / np.log(2))
+
+    ax.patch.set_facecolor("gray")
+    ax.set_aspect("equal", "box")
+    ax.xaxis.set_major_locator(plt.NullLocator())
+    ax.yaxis.set_major_locator(plt.NullLocator())
+
+    for (x, y), w in np.ndenumerate(matrix):
+        color = BLUE if w > 0 else RED
+        size = np.sqrt(np.abs(w) / max_weight)
+        rect = plt.Rectangle(
+            [x - size / 2, y - size / 2], size, size, facecolor=color, edgecolor=color
+        )
+        ax.add_patch(rect)
+
+    ax.autoscale_view()
+    if filename is not None:
+        plt.savefig(f"{filename}.svg")
