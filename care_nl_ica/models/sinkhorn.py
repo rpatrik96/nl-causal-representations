d336a3d829b664eb4c2c0e03ab2ce5653288e866
Patrik Reizinger <prmedia17@gmail.com>
2022-10-09 20:06:50 +0100

    rank accuracy sinkhorn

--- care_nl_ica/models/sinkhorn.py
+++ care_nl_ica/models/sinkhorn.py
@@ -1,5 +1,6 @@
 import torch
 from torch import nn as nn
+from care_nl_ica.losses.dep_mat import permutation_loss
 
 
 class SinkhornOperator(object):
@@ -82,6 +83,7 @@ def learn_permutation(
     dag_permute: bool = True,
     eps=1e-8,
     row_normalize: bool = False,
+    rank_acc=False,
 ):
     est_jac = torch.from_numpy(est_jac).float()
     true_jac = torch.from_numpy(true_jac).float()
@@ -148,37 +150,62 @@ def learn_permutation(
         )
     else:
         optim = torch.optim.Adam(s_ica.parameters(), lr=lr)
-    if True or dag_permute is True:
-        for i in range(num_steps):
+    for i in range(num_steps):
 
-            optim.zero_grad()
-            if dag_permute is True:
-                matrix = (
-                    s_ica.doubly_stochastic_matrix
-                    @ est_jac.abs()
-                    @ s_dag.doubly_stochastic_matrix
+        optim.zero_grad()
+        if dag_permute is True:
+            matrix = (
+                s_ica.doubly_stochastic_matrix
+                @ est_jac.abs()
+                @ s_dag.doubly_stochastic_matrix
+            )
+        else:
+            matrix = s_ica.doubly_stochastic_matrix @ est_jac.abs()
+        loss_l = -tril_weight * torch.tril(matrix, 0).abs().sum()
+        loss_u = triu_weigth * torch.triu(matrix, 1).abs().sum()
+        loss_diag = diag_weight * (1.0 / (matrix.diag() + eps)).sum()
+
+        loss = loss_l + loss_u + loss_diag
+
+        loss.backward()
+
+        if i % 250 == 0 and dag_permute is True:
+            correct_order = torch.all(
+                s_dag.doubly_stochastic_matrix.max(1)[1]
+                == torch.tensor(permute_indices)
+            ).item()
+            if correct_order is True:
+                if verbose is True:
+                    print("Correct order identified")
+                return (
+                    True,
+                    j_hamming(true_jac, matrix),
+                    j_acc(true_jac, matrix),
+                    permutation_loss(s_dag.doubly_stochastic_matrix).item(),
+                    permutation_loss(s_ica.doubly_stochastic_matrix).item(),
                 )
-            else:
-                matrix = s_ica.doubly_stochastic_matrix @ est_jac.abs()
-            loss_l = -tril_weight * torch.tril(matrix, 0).abs().sum()
-            loss_u = triu_weigth * torch.triu(matrix, 1).abs().sum()
-            loss_diag = diag_weight * (1.0 / (matrix.diag() + eps)).sum()
 
-            loss = loss_l + loss_u + loss_diag
+        optim.step()
 
-            loss.backward()
+    learned_order = s_dag.doubly_stochastic_matrix.max(1)[1]
+    correct_order = torch.tensor(permute_indices)
 
-            if i % 250 == 0 and dag_permute is True:
-                correct_order = torch.all(
-                    s_dag.doubly_stochastic_matrix.max(1)[1]
-                    == torch.tensor(permute_indices)
-                ).item()
-                if correct_order is True:
-                    if verbose is True:
-                        print("Correct order identified")
-                    return (True, j_hamming(true_jac, matrix), j_acc(true_jac, matrix))
+    indices = torch.arange(dim)
 
-            optim.step()
+    # calculate the ratio of index pairs that are in the correct order
+    correct_rank_pairs = 0.0
+    if len(learned_order.unique()) == dim:
+
+        for o1 in range(dim):
+            for o2 in range(o1 + 1, dim):
+                correct_rank_pairs += (
+                    indices[learned_order == o1] - indices[learned_order == o2]
+                ).sign() == (
+                    indices[correct_order == o1] - indices[correct_order == o2]
+                ).sign()
+    else:
+        correct_rank_pairs = -1
+
     if verbose is True:
         print("----------------------------------")
         print(f"{true_jac=}")
@@ -195,4 +222,10 @@ def learn_permutation(
         if dag_permute is True:
             print(f"S_DAG={s_dag.doubly_stochastic_matrix.detach()}")
         print(f"S_ICA={s_ica.doubly_stochastic_matrix.detach()}")
-    return (False, j_hamming(true_jac, matrix), j_acc(true_jac, matrix))
+    return (
+        False if rank_acc is False else correct_rank_pairs / (dim * (dim - 1) / 2.0),
+        j_hamming(true_jac, matrix),
+        j_acc(true_jac, matrix),
+        permutation_loss(s_dag.doubly_stochastic_matrix).item(),
+        permutation_loss(s_ica.doubly_stochastic_matrix).item(),
+    )
