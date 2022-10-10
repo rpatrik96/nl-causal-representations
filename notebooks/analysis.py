54249b27c6d8ef1aa3807d60eaccf79007c464a6
Patrik Reizinger <prmedia17@gmail.com>
2022-10-10 14:13:18 +0100

    analysis cleanup + extended acc order prints

--- notebooks/analysis.py
+++ notebooks/analysis.py
@@ -17,7 +17,6 @@ BLUE = "#1A85FF"
 RED = "#D0021B"
 METRIC_EPS = 1e-6
 
-from matplotlib import pyplot as plt
 from matplotlib import rc
 
 
@@ -217,76 +216,7 @@ def sweep2df(
     )
 
 
-def format_violin(vp, facecolor=BLUE):
-    for el in vp["bodies"]:
-        el.set_facecolor(facecolor)
-        el.set_edgecolor("black")
-        el.set_linewidth(0.75)
-        el.set_alpha(0.9)
-    for pn in ["cbars", "cmins", "cmaxes", "cmedians"]:
-        vp_ = vp[pn]
-        vp_.set_edgecolor("black")
-        vp_.set_linewidth(0.5)
-
-
 import matplotlib.pyplot as plt
-
-
-def create_violinplot(groups, xlabel, ylabel, xticklabels, filename=None, ax=None):
-    if ax is None:
-        fig, ax = plt.subplots()
-    else:
-        ax = ax.twinx()
-
-    vp = ax.violinplot(groups, showmedians=True)
-    format_violin(vp, BLUE)
-
-    ax.set_xticklabels(xticklabels)
-    # ax.set_xticks(xticks)
-    # plt.locator_params(axis='y', nbins=5)
-    # plt.yticks(fontsize=24)
-    # plt.ylim([0, 0.5])
-    ax.set_ylabel(ylabel)
-    # ax.set_xlabel(xlabel)
-    if filename is not None:
-        plt.savefig(f"{filename}.svg")
-    return ax
-
-
-def violin_by_prior(
-    gauss_data,
-    laplace_data,
-    uniform_data,
-    xticks,
-    xlabel,
-    ylabel,
-    offset,
-    filename,
-    figsize=(8, 6),
-    log=False,
-):
-    plt.figure(figsize=figsize)
-    vp_gauss = plt.violinplot(
-        [np.log10(i) if log is True else i for i in gauss_data], positions=xticks
-    )
-    vp_laplace = plt.violinplot(
-        [np.log10(i) if log is True else i for i in laplace_data],
-        positions=-offset + xticks,
-    )
-    vp_uniform = plt.violinplot(
-        [np.log10(i) if log is True else i for i in uniform_data],
-        positions=offset + xticks,
-    )
-    plt.legend(
-        [vp_gauss["bodies"][0], vp_laplace["bodies"][0], vp_uniform["bodies"][0]],
-        ["gaussian", "laplace", "uniform"],
-        loc="upper right",
-    )
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
-    plt.xticks(xticks)
-    # plt.tight_layout()
-    plt.savefig(filename)
 
 
 def learning_stats(
@@ -306,10 +236,8 @@ def learning_stats(
     for dim in df.dim.unique():
         for selector in df[selector_col].unique():
             success = []
-            hamming = []
+            hamming_dist = []
             accuracy = []
-            permutation_losses_dag = []
-            permutation_losses_ica = []
             for (selector_item, j_gt, j_est, permute) in zip(
                 df[selector_col],
                 true_unmix_jacobians,
@@ -335,19 +263,19 @@ def learning_stats(
                         rank_acc=rank_acc,
                     )
 
-                    print(s, p_dag, p_ica)
                     if s >= 0:
                         success.append(s)
-                        hamming.append(h)
+                        hamming_dist.append(h)
                         accuracy.append(a)
-                        permutation_losses_dag.append(p_dag)
-                        permutation_losses_ica.append(p_ica)
 
             mcc = df.mcc[(df.dim == dim) & (df[selector_col] == selector)]
+            success = np.array(success)
+            accuracy = np.array(accuracy)
+            hamming_dist = np.array(hamming_dist)
             print("----------------------------------")
             if len(success) > 0:
                 print(
-                    f"{dim=} ({selector_col}={selector})\tMCC={mcc.mean():.3f}+{mcc.std():.3f}\tAcc(order):{np.array(success).mean():.3f}\t  Acc:{np.array(accuracy).mean():.3f}\tSHD:{np.array(hamming).mean():.6f}\t[{len(success)} items]"
+                    f"{dim=} ({selector_col}={selector})\tMCC={mcc.mean():.3f}+{mcc.std():.3f}\t  Acc:{accuracy.mean():.3f}+{accuracy.std():.3f}\tAcc(order):{success.mean():.3f}+{success.std():.3f}\tOrder ratio:{len(success)/len(mcc):.3f}\tSHD:{hamming_dist.mean():.6f}+{hamming_dist.std():.6f}\t[{len(mcc)} items]"
                 )
             else:
                 print(f"No experiments for {dim=} ({selector_col}={selector})")
